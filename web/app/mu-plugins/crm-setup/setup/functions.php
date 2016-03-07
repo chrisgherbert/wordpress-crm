f565f2fc857c9768440e343395e1b75e447a8123
Chris Herbert <herbert@bermanco.com>
2016-03-07 11:24:34 -0500

    Added preliminary code that forwards people back to the front end after they delete a record.

--- web/app/mu-plugins/crm-setup/setup/functions.php
+++ web/app/mu-plugins/crm-setup/setup/functions.php
@@ -6,3 +6,29 @@ function filter_by_security_groups(){
 
 
 }
+
+add_action('deleted_post', 'redirect_after_after_deleting_records', 10, 1);
+
+function redirect_after_after_deleting_records($post_id){
+
+	$accepted_post_types = array(
+		'contact',
+		'organization'
+	);
+
+	$post = Timber::get_post($post_id);
+
+	if (in_array($post->post_type, $accepted_post_types)){
+
+		if (!session_id()){
+			@session_start();
+		}
+
+		$msg = new \Plasticbrain\FlashMessages\FlashMessages();
+		$msg->error('Deleted ' . $post->title);
+		wp_redirect('/contact');
+		exit;
+
+	}
+
+}
